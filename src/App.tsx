712e5ea2ee46faa1f8a63b4d113bf3ba9d5cc18f
vitoriaacaroline <134931502+vitoriaacaroline@users.noreply.github.com>
2023-05-29 08:05:22 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import {useState} from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png'
+
+const App = () => {
+    
+     const [altura, setAltura] = useState (0);
+     const [peso, setPeso] = useState (0);
+
+     const handleCalc = () => {
+       if (altura && peso) {
+
+       }
+       else {
+         alert("Informe a Altura e o Peso")
+       }
+   }
+
+
+   return(
+     <div className={styles.main}>
+       <header>
+          <div className={styles.headerContainer}>
+             <img src={poweredImage} alt="" width={150} />
+          </div>
+       </header>
+       <div className={styles.container}>
+         <div className={styles.leftSide}>
+           <h1> Calculo do IMC</h1>
+           <p> O IMC (Índice de Massa Corporal) é reconhecido pela (OMS) Organização Mundial da Saúde como um padrão internacional que avalia se as pessoas, entre 20 e 59 anos, estão com peso ideal ou em excesso, em relação a sua altura.</p>
+         <input 
+           type="number"
+           placeholder="Informe a altura. Ex. 1.5 (em metro)"
+           value={altura > 0 ? altura:''}
+           onChange={t => setAltura(parseFloat(t.target.value) )}
+         />
+
+         <input 
+           type="number"
+           placeholder="Informe o peso. Ex. 60.5 (em kilo)"
+           value={peso > 0 ? peso:''}
+           onChange={t => setPeso(parseFloat(t.target.value) )}
+         />
+
+         <button onClick={handleCalc}> Calcular </button>
+         
+         </div>
+
+         <div className={styles.rightSide}>
+          ...
+       </div>
+    </div>
+
+    </div>
+   )
+}
+
+export default App;
---
+++ src/helpers/imc.ts
@@ -0,0 +1,26 @@
+export type Level = {
+    title: string;
+    color: string;
+    icon: 'down' | 'up';
+    imc: number[];
+    yourImc?: number;
+}
+
+export const levels: Level[] = [
+  {title: 'Magreza',   color:'#37adab',   icon:'down',  imc:[0, 18.5]},
+  {title: 'Normal',    color:'#27ba47',   icon:'up',    imc:[18.6, 24.9]},
+  {title: 'Sobrepeso', color:'#d99f2b',   icon:'down',  imc:[25, 30]},
+  {title: 'Obesidade', color:'#e3c427',   icon:'down',  imc:[30.1, 99]}
+];
+
+export const calculateImc = (altura:number, peso:number) => {
+  const imc = peso / (altura * altura);
+
+  for ( let i in levels) {
+    if ( imc >= levels[i].imc[0] && imc <= levels[i].imc[1]){
+       levels[i].yourImc = imc;
+       return levels[i]
+    }
+  }
+  return null;
+}
